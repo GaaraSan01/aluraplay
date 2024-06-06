230e1f756706fa4b4b1e469ef44186c83212d3fc
Vinicius Henrique <vini2003henri12395@gmail.com>
2024-06-05 21:45:50 -0300

    API in Json

--- config/router.php
+++ config/router.php
@@ -1,13 +1,18 @@
 <?php
 
-use Alura\Mvc\Controller\EditVideoController;
-use Alura\Mvc\Controller\FormVideoController;
-use Alura\Mvc\Controller\LoginController;
-use Alura\Mvc\Controller\LoginFormController;
-use Alura\Mvc\Controller\LogoutController;
-use Alura\Mvc\Controller\NovoVideoController;
-use Alura\Mvc\Controller\RemoveVideoController;
-use Alura\Mvc\Controller\VideoListController;
+use Alura\Mvc\Controller\{
+    EditVideoController,
+    FormVideoController,
+    JsonVideosListController,
+    LoginController,
+    LoginFormController,
+    LogoutController,
+    NewJsonVideoController,
+    NovoVideoController,
+    RemoveVideoController,
+    VideoListController
+};
+
 
 return [
     'GET|/' => VideoListController::class,
@@ -19,4 +24,6 @@ return [
     'GET|/login' => LoginFormController::class,
     'POST|/login' => LoginController::class,
     'GET|/logout' => LogoutController::class,
+    'GET|/videos-json' => JsonVideosListController::class,
+    'POST|/videos'=> NewJsonVideoController::class,
 ];
